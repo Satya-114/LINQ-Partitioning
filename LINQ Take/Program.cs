6145887ef782ff66b3697a181ccbbab142f67f24
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-11 20:20:58 +0530

    Add files via upload

---
+++ LINQ Take/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Take
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            List<int> ResultMS = numbers.Take(4).ToList();
+
+            foreach (var num in ResultMS)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
