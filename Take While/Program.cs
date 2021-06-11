9b161d12f359e7e7ec36447b6d7f5e3f3b982b45
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-11 20:24:12 +0530

    Add files via upload

---
+++ Take While/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TakeWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            List<int> ResultMS = numbers.TakeWhile(num => num < 6).ToList();
+
+            foreach (var num in ResultMS)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
